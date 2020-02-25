90a56cdffdc11e7f12a62033c428079dca6dc20b
Muhammed Salih <salih@toobler.com>
2020-02-25 17:53:38 +0530

    update

--- iosutils/Classes/Extention/EXAttributedString.swift
+++ iosutils/Classes/Extention/EXAttributedString.swift
@@ -4,7 +4,7 @@ import UIKit
 
 extension NSMutableAttributedString{
     func setColorForText(_ textToFind: String?, with color: UIColor) {
-        
+
         let range:NSRange?
         if let text = textToFind{
             range = self.mutableString.range(of: text, options: .caseInsensitive)
@@ -23,34 +23,35 @@ extension NSMutableAttributedString{
             range = NSMakeRange(0, self.length)
         }
         if range!.location != NSNotFound {
-            addAttribute(NSAttributedString.Key.underlineStyle, value:NSUnderlineStyle.styleThick.rawValue, range: range!)
+            addAttribute(NSAttributedString.Key.underlineStyle, value:NSUnderlineStyle.thick.rawValue, range: range!)
             addAttribute(NSAttributedString.Key.underlineColor, value:color , range: range!)
         }
     }
     func setHighlightColorForText(_ textToFind:String?,with color:UIColor){
-        
+
         let range:NSRange?
         if let text = textToFind{
             range = self.mutableString.range(of: text, options: .caseInsensitive)
         }else{
             range = NSMakeRange(0, self.length)
         }
-        
+
         if range!.location != NSNotFound {
             addAttribute(NSAttributedString.Key.backgroundColor, value: color, range: range!)
         }
     }
     func setFont(_ textToFind:String?,withFont:UIFont){
-        
+
         let range:NSRange?
         if let text = textToFind{
             range = self.mutableString.range(of: text, options: .caseInsensitive)
         }else{
             range = NSMakeRange(0, self.length)
         }
-        
+
         if range!.location != NSNotFound {
             addAttribute(NSAttributedString.Key.font , value: withFont, range: range!);
         }
     }
 }
+
