d7e2a2920cc9f8c5bc0c2acf6773c701245f49d6
Muhammed Salih <mzalih@gmail.com>
2020-02-25 22:35:41 +0530

    rename thickvalue

--- iosutils/Classes/Extention/EXAttributedString.swift
+++ iosutils/Classes/Extention/EXAttributedString.swift
@@ -15,7 +15,8 @@ public extension NSMutableAttributedString{
             addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range!)
         }
     }
-    func setUnderlineWith(_ textToFind:String?, with color: UIColor){
+    func setUnderlineWith(_ textToFind:String?, with color: UIColor,style:Any){
+        //NSUnderlineStyle.styleThick.rawValue
         let range:NSRange?
         if let text = textToFind{
             range = self.mutableString.range(of: text, options: .caseInsensitive)
@@ -23,7 +24,7 @@ public extension NSMutableAttributedString{
             range = NSMakeRange(0, self.length)
         }
         if range!.location != NSNotFound {
-            addAttribute(NSAttributedString.Key.underlineStyle, value:NSUnderlineStyle.styleThick.rawValue, range: range!)
+            addAttribute(NSAttributedString.Key.underlineStyle, value:style, range: range!)
             addAttribute(NSAttributedString.Key.underlineColor, value:color , range: range!)
         }
     }
